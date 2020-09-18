d3249fa004c6a524fc15d99eb3ce8e3047bedf4d
mark0802-prog <hayabua.deu.horn@live.jp>
2020-09-19 00:08:15 +0900

    正誤に合わせて音が出る機能を修正

--- app/javascript/quiz.js
+++ app/javascript/quiz.js
@@ -6,8 +6,8 @@ function quiz() {
   const note = ["E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4"]
   const answers = document.querySelectorAll(".answer")
   const correct = document.getElementById("correct")
-  const chime_correct = new Tone.Player('https://github.com/mark0802-prog/pitch_test/tree/master/app/assets/audios/se_maoudamashii_onepoint14.mp3').toDestination
-  const chime_wrong = new Tone.Player('https://github.com/mark0802-prog/pitch_test/tree/master/app/assets/audios/se_maoudamashii_onepoint15.mp3').toDestination
+  const chime_correct = document.getElementById("chime_correct")
+  const chime_wrong = document.getElementById("chime_wrong")
 
   button.addEventListener("click", () => {
     this.note_random = note[Math.floor(Math.random()*note.length)]
@@ -20,11 +20,11 @@ function quiz() {
       const note_answer = answer.innerHTML
       if (note_answer==note_random) {
         correct.innerHTML = '<p>Correct!</p>'
-        chime_correct.start()
+        chime_correct.play()
         console.log("a")
       } else {
         correct.innerHTML = '<p>Wrong!</p>'
-        chime_wrong.start()
+        chime_wrong.play()
         console.log("b")
       }
     }
