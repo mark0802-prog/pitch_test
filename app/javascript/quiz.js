c5f86938c7b787c47a7dc2e88d829c2240088fb5
mark0802-prog <hayabua.deu.horn@live.jp>
2020-09-21 20:55:56 +0900

    正答率などのスコアを表示する機能の実装

--- app/javascript/quiz.js
+++ app/javascript/quiz.js
@@ -2,28 +2,38 @@ import * as Tone from 'tone'
 
 function quiz() {
   const synth = new Tone.Synth().toDestination()
-  const button = document.getElementById("button")
-  const again = document.getElementById("again")
+  const correct_count = document.getElementById("correct_count")
+  const wrong_count = document.getElementById("wrong_count")
+  const correct_rate = document.getElementById("correct_rate")
+  const total_time = document.getElementById("total_time")
+  const average_time = document.getElementById("average_time")
+  const start_button = document.getElementById("start_button")
+  const again_button = document.getElementById("again_button")
   const note = ["E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3", "C4", "C#4", "D4", "D#4"]
-  const answers = document.querySelectorAll(".answer")
+  const answers = document.querySelectorAll(".answer_button")
   const correct = document.getElementById("correct")
   const chime_correct = document.getElementById("chime_correct")
   const chime_wrong = document.getElementById("chime_wrong")
 
-  again.style.display = "none"
+  again_button.style.display = "none"
 
-  button.addEventListener("click", () => {
+  start_button.addEventListener("click", () => {
+    if (!total_time.getAttribute("set")) {
+      setInterval(() => total_time.innerHTML = Math.floor(Tone.now()), 1000);
+    }
+    total_time.setAttribute("set", true)
+
     this.note_random = note[Math.floor(Math.random()*note.length)]
     synth.triggerAttackRelease(note_random, "4n")
     answers.forEach((answer) => {
       answer.style.visibility = "visible"
     })
-    button.style.display = "none"
-    again.style.display = "inline"
+    start_button.style.display = "none"
+    again_button.style.display = "inline"
     console.log(note_random) //cheat
   })
 
-  again.addEventListener("click", () => {
+  again_button.addEventListener("click", () => {
     synth.triggerAttackRelease(note_random, "4n")
   })
 
@@ -33,14 +43,19 @@ function quiz() {
       const note_answer = answer.innerHTML
       if (note_answer==note_random) {
         correct.innerHTML = '<p>Correct!</p>'
+        correct_count.innerHTML = parseInt(correct_count.innerHTML) + 1
+        correct_rate.innerHTML = Math.round(parseInt(correct_count.innerHTML) / (parseInt(correct_count.innerHTML) + parseInt(wrong_count.innerHTML)) * 100)
+        average_time.innerHTML = Math.round(parseInt(total_time.innerHTML) / parseInt(correct_count.innerHTML))
         chime_correct.play()
-        button.style.display = "inline"
-        again.style.display = "none"
+        start_button.style.display = "inline"
+        again_button.style.display = "none"
         answers.forEach((answer) => {
           answer.style.visibility = "hidden"
         })
       } else {
         correct.innerHTML = '<p>Wrong!</p>'
+        wrong_count.innerHTML = parseInt(wrong_count.innerHTML) + 1
+        correct_rate.innerHTML = Math.round(parseInt(correct_count.innerHTML) / (parseInt(correct_count.innerHTML) + parseInt(wrong_count.innerHTML)) * 100)
         chime_wrong.play()
       }
     }
@@ -49,5 +64,5 @@ function quiz() {
 }
 
 if (document.URL.match(/quizes/)) {
-  addEventListener("load", quiz)
+  addEventListener("DOMContentLoaded", quiz)
 }
