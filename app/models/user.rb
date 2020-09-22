3103c82bad8c7f1fcff864ff91ce8e096fa6e092
mark0802-prog <hayabua.deu.horn@live.jp>
2020-09-22 20:07:23 +0900

    ユーザ登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, uniqueness: true, presence: true
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 end
