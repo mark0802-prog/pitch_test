104cd9fe9834bff71b8542413dfb6ae78df19e66
mark0802-prog <hayabua.deu.horn@live.jp>
2020-09-24 11:43:10 +0900

    スコア管理機能のモデル・コントローラ・jsをリファクタリング

--- app/controllers/quizes_controller.rb
+++ app/controllers/quizes_controller.rb
@@ -3,43 +3,18 @@ class QuizesController < ApplicationController
   def index
     return unless user_signed_in?
 
-    @total_score = UserScore.new(user_id: current_user.id).find_total
-    @daily_score = UserScore.new(user_id: current_user.id).find_daily
-    if @total_score.nil?
-      @total_score = UserScore.new(user_id: current_user.id)
-      if @total_score.save_total
-        redirect_to quizes_path and return
-      else
-        redirect_to root_path and return
-      end
-    end
-    if @daily_score.nil?
-      @daily_score = UserScore.new(user_id: current_user.id)
-      if @daily_score.save_daily
-        redirect_to quizes_path and return
-      else
-        redirect_to root_path and return
-      end
-    end
-    @scores =  UserScore.new(user_id: current_user.id).find
+    @scores = UserScore.new(user_id: current_user.id).first_or_create
   end
 
   def update
-    @total_score = UserScore.new(user_id: current_user.id).find_total
-    @daily_score = UserScore.new(user_id: current_user.id).find_daily
-    redirect_to quizes_path and return if @daily_score.nil?
-    
-    redirect_to root_path and return unless @total_score.update(params_total_score)
-    redirect_to root_path and return unless @daily_score.update(params_daily_score)
+    bool = UserScore.new(params_scores).update
+    redirect_to quizes_path unless bool
   end
 
   private
 
-  def params_total_score
-    params.require(:user_score).permit(:total_correct_count, :total_wrong_count, :total_time)
-  end
-
-  def params_daily_score
-    params.require(:user_score).permit(:daily_correct_count, :daily_wrong_count, :daily_time)
+  def params_scores
+    params.require(:user_score).permit(:total_correct_count, :total_wrong_count, :total_time,
+                                       :daily_correct_count, :daily_wrong_count, :daily_time).merge(user_id: current_user.id)
   end
 end
--- app/javascript/quiz.js
+++ app/javascript/quiz.js
@@ -1,5 +1,9 @@
 import * as Tone from 'tone'
 
+function calc_rate(x, y) {
+  return Math.round(parseInt(x) / (parseInt(x) + parseInt(y)) * 100)
+}
+
 function quiz() {
   const synth = new Tone.Synth().toDestination()
   const total_correct_count = document.getElementById("total_correct_count")
@@ -24,15 +28,15 @@ function quiz() {
   const submit_buttons = document.querySelectorAll("#submit")
 
   again_button.style.display = "none"
-  total_correct_rate.innerHTML = Math.round(parseInt(total_correct_count.value) / (parseInt(total_correct_count.value) + parseInt(total_wrong_count.value)) * 100)
+  total_correct_rate.innerHTML = calc_rate(total_correct_count.value, total_wrong_count.value)
   total_average_time.innerHTML = Math.round(parseInt(total_time.value) / parseInt(total_correct_count.value))
-  daily_correct_rate.innerHTML = Math.round(parseInt(daily_correct_count.value) / (parseInt(daily_correct_count.value) + parseInt(daily_wrong_count.value)) * 100)
+  daily_correct_rate.innerHTML = calc_rate(daily_correct_count.value, daily_wrong_count.value)
   daily_average_time.innerHTML = Math.round(parseInt(daily_time.value) / parseInt(daily_correct_count.value))
 
   start_button.addEventListener("click", () => {
     const now = Tone.now()
-    this.total_intervalID = setInterval(() => total_time.value = current_total_time + Math.floor(Tone.now() - now), 1000)
-    this.daily_intervalID = setInterval(() => daily_time.value = current_daily_time + Math.floor(Tone.now() - now), 1000)
+    this.total_intervalID = setInterval(() => total_time.value = current_total_time + Math.floor(Tone.now() - now), 100)
+    this.daily_intervalID = setInterval(() => daily_time.value = current_daily_time + Math.floor(Tone.now() - now), 100)
 
     this.note_random = note[Math.floor(Math.random()*note.length)]
     synth.triggerAttackRelease(note_random, "4n")
@@ -55,10 +59,10 @@ function quiz() {
       if (note_answer==note_random) {
         correct.innerHTML = '<p>Correct!</p>'
         total_correct_count.value = parseInt(total_correct_count.value) + 1
-        total_correct_rate.innerHTML = Math.round(parseInt(total_correct_count.value) / (parseInt(total_correct_count.value) + parseInt(total_wrong_count.value)) * 100)
+        total_correct_rate.innerHTML = calc_rate(total_correct_count.value, total_wrong_count.value)
         total_average_time.innerHTML = Math.round(parseInt(total_time.value) / parseInt(total_correct_count.value))
         daily_correct_count.value = parseInt(daily_correct_count.value) + 1
-        daily_correct_rate.innerHTML = Math.round(parseInt(daily_correct_count.value) / (parseInt(daily_correct_count.value) + parseInt(daily_wrong_count.value)) * 100)
+        daily_correct_rate.innerHTML = calc_rate(daily_correct_count.value, daily_wrong_count.value)
         daily_average_time.innerHTML = Math.round(parseInt(daily_time.value) / parseInt(daily_correct_count.value))
         chime_correct.play()
         if (submit_buttons != null) {
@@ -78,9 +82,9 @@ function quiz() {
       } else {
         correct.innerHTML = '<p>Wrong!</p>'
         total_wrong_count.value = parseInt(total_wrong_count.value) + 1
-        total_correct_rate.innerHTML = Math.round(parseInt(total_correct_count.value) / (parseInt(total_correct_count.value) + parseInt(total_wrong_count.value)) * 100)
+        total_correct_rate.innerHTML = calc_rate(total_correct_count.value, total_wrong_count.value)
         daily_wrong_count.value = parseInt(daily_wrong_count.value) + 1
-        daily_correct_rate.innerHTML = Math.round(parseInt(daily_correct_count.value) / (parseInt(daily_correct_count.value) + parseInt(daily_wrong_count.value)) * 100)
+        daily_correct_rate.innerHTML = calc_rate(daily_correct_count.value, daily_wrong_count.value)
         chime_wrong.play()
         if (submit_buttons != null) {
           submit_buttons.forEach((submit_button) => {
