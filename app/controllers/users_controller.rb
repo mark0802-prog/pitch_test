af703f8c1722e3f0b4615a4c3a0d062df1875bc5
mark0802-prog <hayabua.deu.horn@live.jp>
2020-09-25 14:10:25 +0900

    totalとdailyの正答数ランキングの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def index
+    @total_scores = TotalScore.includes(:user).order(total_correct_count: :DESC)
+    @daily_scores = DailyScore.includes(:user).where(created_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day).order(daily_correct_count: :DESC)
   end
 end
