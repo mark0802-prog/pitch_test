633ca52e5cf3f42923143d0bc20c52275396c1b6
mark0802-prog <hayabua.deu.horn@live.jp>
2020-09-28 20:35:42 +0900

    seed値を調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ User.where("nickname like 'test%'").each do |user|
   total_correct_count = rand(1...100)
   total_wrong_count = rand(10...100)
   total_correct_rate = (total_correct_count.to_f / (total_correct_count + total_wrong_count) * 100).round
-  total_time = rand(100..500)
+  total_time = rand(200..500)
   total_average_time = (total_time.to_f / total_correct_count).round(2)
   total_scores << TotalScore.new(user_id: user.id,
                                  total_correct_count: total_correct_count,
@@ -20,10 +20,10 @@ User.where("nickname like 'test%'").each do |user|
                                  total_time: total_time,
                                  total_average_time: total_average_time)
 
-  daily_correct_count = rand(1...100)
-  daily_wrong_count = rand(1...100)
+  daily_correct_count = rand(1...20)
+  daily_wrong_count = rand(2...20)
   daily_correct_rate = (daily_correct_count.to_f / (daily_correct_count + daily_wrong_count) * 100).round
-  daily_time = rand(200..500)
+  daily_time = rand(40..100)
   daily_average_time = (daily_time.to_f / daily_correct_count).round(2)
   daily_scores << DailyScore.new(user_id: user.id,
                                  daily_correct_count: daily_correct_count,
