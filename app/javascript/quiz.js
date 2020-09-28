c9550e7eb79a1527cdbd79fca6ab9442f15c6a3c
mark0802-prog <hayabua.deu.horn@live.jp>
2020-09-28 20:26:51 +0900

    timeの取得をfloatに修正

--- app/javascript/quiz.js
+++ app/javascript/quiz.js
@@ -10,13 +10,13 @@ function quiz() {
   const total_wrong_count = document.getElementById("total_wrong_count")
   const total_correct_rate = document.getElementById("total_correct_rate")
   const total_time = document.getElementById("total_time")
-  let current_total_time = parseInt(total_time.value)
+  let current_total_time = parseFloat(total_time.value)
   const total_average_time = document.getElementById("total_average_time")
   const daily_correct_count = document.getElementById("daily_correct_count")
   const daily_wrong_count = document.getElementById("daily_wrong_count")
   const daily_correct_rate = document.getElementById("daily_correct_rate")
   const daily_time = document.getElementById("daily_time")
-  let current_daily_time = parseInt(daily_time.value)
+  let current_daily_time = parseFloat(daily_time.value)
   const daily_average_time = document.getElementById("daily_average_time")
   const start_button = document.getElementById("start_button")
   const again_button = document.getElementById("again_button")
@@ -29,14 +29,14 @@ function quiz() {
 
   again_button.style.display = "none"
   total_correct_rate.value = calc_rate(total_correct_count.value, total_wrong_count.value)
-  total_average_time.value = Math.round(parseInt(total_time.value) / parseInt(total_correct_count.value) * 100) / 100
+  total_average_time.value = Math.round(parseFloat(total_time.value) / parseInt(total_correct_count.value) * 100) / 100
   daily_correct_rate.value = calc_rate(daily_correct_count.value, daily_wrong_count.value)
-  daily_average_time.value = Math.round(parseInt(daily_time.value) / parseInt(daily_correct_count.value) * 100) / 100
+  daily_average_time.value = Math.round(parseFloat(daily_time.value) / parseInt(daily_correct_count.value) * 100) / 100
 
   start_button.addEventListener("click", () => {
     const now = Tone.now()
-    this.total_intervalID = setInterval(() => total_time.value = current_total_time + Math.floor((Tone.now() - now) * 10) / 10, 10)
-    this.daily_intervalID = setInterval(() => daily_time.value = current_daily_time + Math.floor((Tone.now() - now) * 10) / 10, 10)
+    this.total_intervalID = setInterval(() => total_time.value = Math.round((current_total_time + Tone.now() - now) * 10) / 10, 10)
+    this.daily_intervalID = setInterval(() => daily_time.value = Math.round((current_daily_time + Tone.now() - now) * 10) / 10, 10)
 
     this.note_random = note[Math.floor(Math.random()*note.length)]
     synth.triggerAttackRelease(note_random, "4n")
@@ -60,10 +60,10 @@ function quiz() {
         correct.innerHTML = '<p>Correct!</p>'
         total_correct_count.value = parseInt(total_correct_count.value) + 1
         total_correct_rate.value = calc_rate(total_correct_count.value, total_wrong_count.value)
-        total_average_time.value = Math.round(parseInt(total_time.value) / parseInt(total_correct_count.value) * 100) / 100
+        total_average_time.value = Math.round(parseFloat(total_time.value) / parseInt(total_correct_count.value) * 100) / 100
         daily_correct_count.value = parseInt(daily_correct_count.value) + 1
         daily_correct_rate.value = calc_rate(daily_correct_count.value, daily_wrong_count.value)
-        daily_average_time.value = Math.round(parseInt(daily_time.value) / parseInt(daily_correct_count.value) * 100) / 100
+        daily_average_time.value = Math.round(parseFloat(daily_time.value) / parseInt(daily_correct_count.value) * 100) / 100
         chime_correct.play()
         if (submit_buttons != null) {
           submit_buttons.forEach((submit_button) => {
