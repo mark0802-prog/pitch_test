1eb863fedb38ca2032da2b7136adebc25f472911
mark0802-prog <hayabua.deu.horn@live.jp>
2020-10-01 20:16:23 +0900

    userモデルのテストコード実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    nickname {Faker::Internet.username}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'ユーザ新規登録' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    it '全ての情報が正しく入力されていれば保存できる' do
+      expect(@user).to be_valid
+    end
+    it 'ニックネームが空だと保存できない' do
+      @user.nickname = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it 'ニックネームが「test」から始まると保存できない' do
+      @user.nickname = "test" + @user.nickname
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname is invalid")
+    end
+    it 'パスワードが空だと保存できない' do
+      @user.password = ""
+      @user.password_confirmation = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+    it 'パスワードが5文字以下だと保存できない' do
+      @user.password = Faker::Internet.password(min_length: 2, max_length: 5)
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+    it 'パスワードとパスワード確認用が一致しないと保存できない' do
+      @user.password_confirmation = FactoryBot.build(:user).password_confirmation
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+  end
+end
